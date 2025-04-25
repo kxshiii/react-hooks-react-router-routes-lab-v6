eb631640bb020b8bd137dff285331740896d659a
kxshiii <brian.gikaru@student.moringaschool.com>
2025-04-25 09:39:21 +0300

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,13 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
